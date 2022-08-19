211d6155dd58e4177f20f60749713434d2f94065
yuki-akiba-jp <rabuafe@gmail.com>
2022-08-19 21:59:55 +0900

    add cpplib

--- packages/competitive_programing/pylib/input_file.py
+++ packages/competitive_programing/pylib/input_file.py
@@ -1,3 +1,15 @@
 with open('./filename', 'r') as f:
-    lines = [line.strip() for line in f.readlines()]
+    input_lines = [line.strip() for line in f.readlines()]
+
+    lines = []
+    for line in input_lines:
+        lines.append(line)
     print(lines)
+    # line = list(map(int, line))
+
+    # input_numlines = [line.strip().split(',') for line in f.readlines()]
+    # numlines = []
+    # for line in input_numlines:
+    #     line = list(map(int, line))
+    #     numlines.append(line)
+    # print(numlines)
