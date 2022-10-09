29709894b8edf6f55e42b509baaeaf4164a9a8ae
yuki-akiba-jp <rabuafe@gmail.com>
2022-10-09 21:50:23 +0900

    add

--- packages/competitive_programing/pylib/main.py
+++ packages/competitive_programing/pylib/main.py
@@ -1,56 +1,40 @@
-
-def integer_to_bit_string(num):
-    bit_string = ''
-    while num:
-        to_append_int = num % 2
-        bit_string += str(to_append_int)
-        num //= 2
-    return bit_string[::-1]
-
-
-def bit_string_to_integer(bit_string):
-    bit_string = bit_string[::-1]
-
-    re = 0
-    powpow = 1
-    for i in range(len(bit_string)):
-        if bit_string[i] == '1':
-            re += powpow
-        powpow *= 2
-    return re
-
-
-def func(n_bit_string):
-    cnt = 0
-    for index in range(len(n_bit_string)):
-        if n_bit_string[index] == '1':
-            cnt += 1
-            if cnt == 2:
-                string = n_bit_string[cnt:]
-
-                break
-
-
 N = int(input())
-n_bit_string = integer_to_bit_string(N)
+a = list(map(int, input().split()))
+a.sort()
+seta = set(a)
+seta = list(seta)
+bucket_len = 3*10**5+10
+bucket = [0]*bucket_len
+maxCnt = bucket_len
+if not 1 in a:
+    print(0)
+    exit()
 
-# N, W = map(int, input().split())
+for num in a:
+    bucket[num] += 1
 
-# w = [0]*N
-# v = [0]*N
-# for i in range(N):
-#     w_in, v_in = map(int, input().split())
-#     w[i] = w_in
-#     v[i] = v_in
+pool = len(a)-len(set(a))
+ans = 0
+for index in range(1, bucket_len):
+    if bucket[index] >= 1:
+        ans += 1
+    elif pool-2 >= 0:
+        pool -= 2
+        ans += 1
+    elif len(seta) >= 2:
+        for _ in range(2):
+            if seta[-1] > index:
+                delnum = seta[-1]
+                seta.pop(-1)
+                bucket[delnum] -= 1
+            else:
+                print(ans)
+                exit()
+
+        ans += 1
+
+    else:
+        break
 
 
-# dp = [[0]*(W+1) for i in range(N+1)]
-
-# for i in range(N):
-#     for j in range(W+1):
-#         if j < w[i]:
-#             dp[i+1][j] = dp[i][j]
-#         else:
-#             dp[i+1][j] = max(dp[i][j], dp[i][j-w[i]]+v[i])
-
-# print(dp[N][W])
+print(ans)
