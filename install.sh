e91fae7dcda2feee2dfb2f79eefedb7890de7008
yuki-akiba-jp <rabuafe@gmail.com>
2022-11-01 21:15:54 +0900

    ini

--- install.sh
+++ install.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
-if [! -f /usr/local/bin/brew ]; then
+if [ ! -f /usr/local/bin/brew ]; then
     /bin/bash -c "$(curl -c https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
-# set -e 
 
 if [ "$(dscl . -read ~/ UserShell)" = "UserShell: /bin/bash" ]; then
     chsh -s $(which zsh)
 fi
 
 if [ ! -d ~/dotfiles ]; then
-cd ~
-git clone https://github.com/yuki-akiba-jp/dotfiles.git
+    cd ~
+    git clone https://github.com/yuki-akiba-jp/dotfiles.git
 fi
 
 brew bundle -v --file=~/dotfiles/Brewfile
@@ -23,4 +22,4 @@ fi
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-stow -v -d ~/dotfiles/packages -t ~ alfred git iterm2 neovim starship tmux zsh miniforge coc-snippets
+stow -v -d ~/dotfiles/packages -t ~ alfred git iterm2 neovim starship tmux zsh  coc-snippets
