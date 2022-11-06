c7bffe18b7236a4b703397212a7ea88266c1a6a0
yuki-akiba-jp <rabuafe@gmail.com>
2022-11-06 15:50:47 +0900

    ini

--- install_for_linuxos.sh
+++ install_for_linuxos.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
+if [ ! -f /usr/local/bin/brew ]; then
+    sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+
 if [ ! -d ~/dotfiles ]; then
     cd ~
     git clone https://github.com/yuki-akiba-jp/dotfiles.git
 fi
 
 chsh -s $(which zsh)
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.zshrc
+
+brew bundle -v --file=~/dotfiles/Brewfile
 
 if [ ! -d ~/.config ]; then
     mkdir ~/.config
@@ -15,4 +23,4 @@ fi
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-stow -v -d ~/dotfiles/packages -t ~  git  neovim starship tmux zsh  coc-snippets docker
+stow -v -d ~/dotfiles/packages -t ~ alfred git iterm2 neovim starship tmux zsh  coc-snippets docker
