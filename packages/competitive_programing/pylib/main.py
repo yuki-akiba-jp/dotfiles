2ae3d2723cd3ff9c029e5f5b1b6ea369e5e421bd
yuki akiba <yukiakiba@mac.local>
2023-01-31 08:12:30 +0900

    edit js snippet

--- packages/competitive_programing/pylib/main.py
+++ packages/competitive_programing/pylib/main.py
@@ -1,3 +1,11 @@
-n, q = map(str, input().split())
-n = int(n)
-s = str(input())
+from bisect import bisect_left
+from collections import defaultdict
+
+# n, q = map(str, input().split())
+# n = int(n)
+# s = str(input())
+
+a = [1, 2, 3]
+i = bisect_left(a, 2j)
+print(a)
+print(i)
