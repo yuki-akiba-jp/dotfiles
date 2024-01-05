883a4d12adcaf0bcce2fc03c4ee2040ed5e3f62c
yuki-akiba-jp <rabuafe@gmail.com>
2024-01-05 15:39:38 +0900

    add lua

--- packages/competitive_programing/pylib/main.py
+++ packages/competitive_programing/pylib/main.py
@@ -2,17 +2,14 @@
 # a = list(map(int, input().split()))
 # s = str(input())
 # a, n = map(int, input().split())
-rows  = []
 
-for i in range(18):
-    lis = list(map(int, input().split()))
-    rows.append(lis)
-
-for i in range(18):
-    cnt = 0
-    for row in rows:
-        cnt+=row[i]
-
-    print(i+1,cnt)
-
-
+base_salary = 32*12*0.8
+bonus = 0
+for experienced_year in range(1, 5):
+    previous_base_salary = base_salary
+    base_salaly = previous_base_salary * 1.06
+    print('base_salaly',int( base_salary ))
+    bonus = base_salary * 4/12
+    print('bonus',int(bonus))
+    print('year salary',int( base_salary + bonus))
+    
