b9ed484af1de622a9317bc96541e5d75e580b613
yuki-akiba-jp <rabuafe@gmail.com>
2024-04-22 19:40:04 +0900

    add: jump placeholder

--- packages/neovim/.config/nvim/lua/plugins/cmp.lua
+++ packages/neovim/.config/nvim/lua/plugins/cmp.lua
@@ -22,12 +22,23 @@ cmp.setup {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
+
+    ["<C-k>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.locally_jumpable(1) then
+        luasnip.jump(1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
   },
-  sources = cmp.config.sources( {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-        { name = 'buffer' },
-        { name = 'path' },
-        { name = 'cmdline' },
-      }),
+
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'buffer' },
+    { name = 'path' },
+    { name = 'cmdline' },
+  }),
 }
