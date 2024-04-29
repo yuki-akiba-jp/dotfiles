3ce8a56f312011f1726db409a6a3d0476ee250e7
yuki-akiba-jp <rabuafe@gmail.com>
2024-04-29 18:46:51 +0900

    add markdown previewer

--- packages/neovim/.config/nvim/lua/plugins/lazy.lua
+++ packages/neovim/.config/nvim/lua/plugins/lazy.lua
@@ -217,4 +217,13 @@ lazy.setup({
       }
     },
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 })
