5fa0060931f5de4d2162c71f3bd160ce00d433fb
yuki-akiba-jp <rabuafe@gmail.com>
2024-08-04 16:32:08 +0900

    add flash nvim

--- packages/neovim/.config/nvim/lua/plugins/lazy.lua
+++ packages/neovim/.config/nvim/lua/plugins/lazy.lua
@@ -208,4 +208,13 @@ lazy.setup({
     'mfussenegger/nvim-jdtls',
     ft = { 'java' },
   },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    },
+  }
 })
